1f3855adc976c58959aa9955f9073593c5968372
Andreas-hiq <andreas.eklund@hiq.se>
2025-09-02 21:52:49 +0200

    Created Butler.Infrastructure project Fixed typos in appsettings

---
+++ Butler/Butler.Infrastructure/InfrastructureExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Butler.Infrastructure
+{
+    public static class InfrastructureExtensions
+    {
+        public static IServiceCollection AddButlerInfrastructure(this IServiceCollection services, IConfiguration config)
+        {
+            string connectionString = config["Db:ConnectionString"]!;
+            NpgsqlDataSourceBuilder builder = new NpgsqlDataSourceBuilder(connectionString);
+            builder.UseVector();
+            NpgsqlDataSource dataSource = builder.Build();
+
+            services.AddSingleton(dataSource);
+
+
+            //TODO: Add other infrastructure services here
+            // e.g services.AddTransient<IRagRepository, RagRepository>(); etc
+
+            return services;
+        }
+    }
+}
