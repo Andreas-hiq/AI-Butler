aa446596afac6a70ac89831b0f0991513d6184b2
Simon Ekeroth <simon.ekeroth@hiq.se>
2025-09-11 12:41:45 +0200

    Add TestHelpers
    - Add TestDatafactory.cs with RandomString method
    - Add MockFactory.cs to easily set up different mocks and respective verifications

---
+++ Butler/Butler.Tests/TestHelpers/MockFactory.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq.Expressions;
+
+namespace Butler.Tests;
+
+using Moq;
+
+public static class MockFactory {
+    public static Mock<T> CreateReturning<T, TResult>(
+        Expression<Func<T, TResult>> expression,
+        TResult result
+    ) where T : class {
+        Mock<T> mock = new Mock<T>();
+        mock.Setup(expression).Returns(result);
+        return mock;
+    }
+
+    public static Mock<T> CreateThrowing<T, TResult>(
+        Expression<Func<T, TResult>> expression, 
+        Exception exception
+    ) where T : class {
+        Mock<T> mock = new Mock<T>();
+        mock.Setup(expression).Throws(exception);
+        return mock;
+    }
+
+    public static void VerifyCalled<T>(
+        Mock<T> mock,
+        Expression<Action<T>> expression,
+        Times? times = null
+    ) where T : class {
+        mock.Verify(expression, times ?? Times.AtLeastOnce());
+    }
+
+    public static void VerifyCalled<T, TResult>(
+        Mock<T> mock,
+        Expression<Func<T, TResult>> expression,
+        Times? times = null
+    ) where T : class {
+        mock.Verify(expression, times ?? Times.AtLeastOnce());
+    }
+}
---
+++ Butler/Butler.Tests/TestHelpers/TestDatafactory.cs
@@ -0,0 +1,7 @@
+﻿namespace Butler.Tests;
+
+public static class TestDatafactory {
+    public static string RandomString(int length) {
+        return new string('x', length);
+    }
+}
