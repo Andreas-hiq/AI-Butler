44aaa7a19d6d620da583fae313f2728f135f51e9
Simon Ekeroth <simon.ekeroth@hiq.se>
2025-09-11 12:11:08 +0200

    Add TestProject to the Solution
    - Connect TestProject to Butler.Core
    - Create TestBase.cs for setup and breakdown of each test
    - Added test for Embedding.Embed

---
+++ Butler/Butler.Tests/RAG/Services/EmbeddingTests.cs
@@ -0,0 +1,19 @@
+﻿using Butler.Core.RAG.Services;
+
+namespace Butler.Tests.RAG.Services;
+
+using Xunit;
+using FluentAssertions;
+
+public class EmbeddingTests : TestBase {
+    
+    [Theory]
+    [InlineData("dummy text")]
+    public async void Test_Embed(string text) {
+        //TODO: Create variable "expected" depicted what SHOULD be returned.
+        float[] expected = Enumerable.Repeat(0.1f, 1536).ToArray();
+        Embedding embedding = new();
+        float[] result = await embedding.Embed(text);
+        result.Should().BeEquivalentTo(expected);
+    }
+}
---
+++ Butler/Butler.Tests/TestHelpers/TestBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Butler.Tests;
+
+public abstract class TestBase : IDisposable
+{
+    public void Testbase() {
+        //TODO: Introduce setup code such as mocks and test data
+    }
+
+    public void Dispose() {
+        //TODO: Common Cleanup code
+    }
+}
